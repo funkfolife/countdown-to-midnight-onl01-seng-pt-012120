850ee55c829303abf2e192fb18709cc1ef7834b0
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-23 23:50:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,13 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
+  until number == 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep 1 
+  end
+  'HAPPY NEW YEAR!"
+  
+end
+
   
